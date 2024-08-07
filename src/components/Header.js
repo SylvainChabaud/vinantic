67856fd7753d6b5b4d4c7f479f1a1ce7c243c2cb
unknown <dsp_fx@hotmail.com>
2024-08-07 16:54:25 +0200

    add logo to main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Title from "./Title";
 import Description from "./Description";
+import logoSrc from '../assets/logo-vinantic.webp';
 
 const Header = () => (
-  <div className="bg-stone-100">
+  <div className="bg-stone-200">
     <div className="py-10">
       <Title />
     </div>
 
-    <div className="pb-14 px-[15vw] border-b-2">
+    <div className="flex justify-center">
+      <img
+        src={logoSrc}
+        alt={'image-logo'}
+        className="w-80 border rounded-full transition duration-1000 ease-in-out transform hover:scale-125"
+      />
+    </div>
+
+    <div className="py-14 px-[15vw] border-b-2">
       <Description />
     </div>
   </div>
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 
 const Title = () => {
   const { t } = useTranslation();
-  return <div className="text-stone-700 font-tangerine font-thin text-9xl text-center">{t('title')}</div>;
+  return <div className="text-[#800020] font-tangerine font-thin text-9xl text-center">{t('title')}</div>;
 };
 
 export default Title;
